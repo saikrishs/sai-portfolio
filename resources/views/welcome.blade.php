b0be883257342a5775c1e3c42b81de47dfe722cb
saikrishs <sai.srungavarapu@gmail.com>
2025-04-16 14:34:36 -0500

    adding header icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -178,7 +178,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href="{{ asset('sai.png') }}" type="image/png">
-  <title>Sai Srungavarapu - Web App Developer</title>
+  <title>Sai Srungavarapu</title>
  <link rel="stylesheet" href="/assets/css/style.css">
 
   <!--
